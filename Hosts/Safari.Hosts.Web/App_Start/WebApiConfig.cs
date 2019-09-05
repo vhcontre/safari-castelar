0e073ec6192490a3216390cecb48f93ecc0df262
Victor H Contreras <vcontreras@gmail.com>
2019-09-05 20:11:34 -0300

    Configuración de la arquitectura de base general (EASafari-arquitectura.pdf).

---
+++ Hosts/Safari.Hosts.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace Safari.Hosts.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var suffix = typeof(DefaultHttpControllerSelector).GetField("ControllerSuffix", BindingFlags.Static | BindingFlags.Public);
+            if (suffix != null) suffix.SetValue(null, string.Empty);
+
+            config.Services.Replace(typeof(IHttpControllerTypeResolver),
+                new HttpServiceTypeResolver());
+
+            config.MapHttpAttributeRoutes();
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+        }
+    }
+}
