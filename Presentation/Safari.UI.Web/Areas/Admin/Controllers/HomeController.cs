c9775479ac11aab256d88a89482cdc33743c173b
Victor H Contreras <vcontreras@gmail.com>
2019-09-05 22:36:27 -0300

    Agregado de ServiceFactory y ILogging

--- Presentation/Safari.UI.Web/Areas/Admin/Controllers/HomeController.cs
+++ Presentation/Safari.UI.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Safari.Framework.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,17 @@ namespace Safari.UI.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
+        private ILoggingService _loggingService;
+
+        public HomeController(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
         // GET: Admin/Home
         public ActionResult Index()
         {
+            _loggingService.Log("message");
             return View();
         }
     }
