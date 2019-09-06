996980c77eb512335fbe9741fd649f9edc2d4b05
Victor H Contreras <vcontreras@gmail.com>
2019-09-05 21:05:58 -0300

    Agregado de Componente Boilerplate - Helper bSEO

---
+++ Presentation/Safari.UI.Web/Constants/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safari.UI.Web.Constants
+{
+    public class Application
+    {
+        public const string Name = "ASP.NET MVC Safari";
+        public const string ShortName = "Safari";
+    }
+}
--- Presentation/Safari.UI.Web/Controllers/HomeController.cs
+++ Presentation/Safari.UI.Web/Controllers/HomeController.cs
@@ -8,22 +8,21 @@ namespace Safari.UI.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("", Name = "HomeControllerRouteIndex")]
         public ActionResult Index()
         {
             return View();
         }
-
+        [Route("acerca-de-nosotros", Name = "HomeControllerRouteAbout")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
+        [Route("contacto", Name = "HomeControllerRouteContact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
